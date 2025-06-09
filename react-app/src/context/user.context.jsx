69a64333d2820155c1199935c8d7d8b855a232c4
Макаров Степан Александрович <st.mkv03@gmail.com>
2025-06-09 15:48:20 +0300

    contextProvider

--- react-app/src/context/user.context.jsx
+++ react-app/src/context/user.context.jsx
@@ -8,5 +8,5 @@ export const UserContext = createContext({
 export const UserContextProvider = ({ children }) => {
   const [userId, setUserId] = useState(1);
 
-  return <UserContext.Provider>{children}</UserContext.Provider>;
+  return <UserContext.Provider value={{ userId, setUserId }}>{children}</UserContext.Provider>;
 };
